e3d933d0bc6da6aaad3ea453810faaa8206f887e
giuliamoretto <giulia.moretto87@gmail.com>
2018-01-24 22:48:14 +0100

    rimozione cartella temp

--- source/presentation/src/travelgo/src/modules/travel/selector.js
+++
@@ -1,8 +0,0 @@
-
-import { createSelector } from 'reselect'
-
-export const getTravelList = (state) => {
-    return state ? state.get('travel') : null;
-}
-
-export const getTravelNames = createSelector( [getTravelList],(travels) => travels ? travels.names : null ); 
